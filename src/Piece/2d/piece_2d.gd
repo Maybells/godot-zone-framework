5b509cd021f9b796b02f090746181c5a815d8244
Maybells <56422340+Maybells@users.noreply.github.com>
2021-04-16 14:33:26 -0700

    Removed extra piece definition

--- src/Piece/2d/piece_2d.gd
+++ src/Piece/2d/piece_2d.gd
@@ -1,6 +1,15 @@
-extends Piece
+extends Node2D
 
 class_name Piece2D
+
+
+var zone: Zone
+var id: int
+var game: GameLogic
+
+
+func return_to_origin():
+	zone.reset_piece_position(self)
 
 
 func warp_to_position(position: Vector2):
