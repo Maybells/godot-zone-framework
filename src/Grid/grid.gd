38745a4a376e0ef857f94c78f18e8c251e6d2509
Maybells <56422340+Maybells@users.noreply.github.com>
2021-04-29 11:00:35 -0700

    Added comments

--- src/Grid/grid.gd
+++ src/Grid/grid.gd
@@ -2,30 +2,37 @@
 class_name Grid
 
 
+# The outer boundaries of the grid
 var boundaries: GridBounds
-var obstacles
+
+# The obstacles currently on the grid
+var _obstacles
 
 
 func _init(boundaries: GridBounds):
 	self.boundaries = boundaries
 
 
+# Returns an array of all obstacles at the given position.
 func _obstacles_at(position) -> Array:
 	var out := []
-	for obstacle in obstacles:
+	for obstacle in _obstacles:
 		if obstacle.position == position:
 			out.append(obstacle)
 	return out
 
 
+# Takes a MovePattern and returns an array of paths described by the pattern.
 func _pattern_to_paths(pattern):
 	pass
 
 
+# Returns the opposite of the given direction.
 func _invert_direction(direction):
 	pass
 
 
+# Creates a new MovePath with values equal to the given MovePath.
 func _duplicate_path(path: MovePath) -> MovePath:
 	var duplicate = MovePath.new(path.instructions)
 	duplicate.path = path.path.duplicate()
@@ -35,6 +42,7 @@ func _duplicate_path(path: MovePath) -> MovePath:
 	return duplicate
 
 
+# Alters the path based on the type and parameters of the given obstacle.
 func _handle_obstacle(path: MovePath, obstacle: GridObstacle):
 	path.add_interaction(obstacle)
 	match obstacle.type:
@@ -47,6 +55,7 @@ func _handle_obstacle(path: MovePath, obstacle: GridObstacle):
 			pass
 
 
+# Interacts with all obstacles at a position who can interact.
 func _handle_obstacles_at(position, path: MovePath, direction, can_interact := "apply_on_enter"):
 	var obs := _obstacles_at(position)
 	for obstacle in obs:
@@ -54,6 +63,7 @@ func _handle_obstacles_at(position, path: MovePath, direction, can_interact := "
 			_handle_obstacle(path, obstacle)
 
 
+# Applies obstacle effects at the end of a path.
 func _handle_end_obstacles(position, path: MovePath):
 	var obs := _obstacles_at(position)
 	for obstacle in obs:
@@ -61,6 +71,7 @@ func _handle_end_obstacles(position, path: MovePath):
 			path.failed = true
 
 
+# Attempts to add a new position to the path.
 func _enter_position(position, path: MovePath, direction):
 	if boundaries.is_in_bounds(position):
 		_handle_obstacles_at(position, path, direction, "apply_on_enter")
@@ -70,6 +81,7 @@ func _enter_position(position, path: MovePath, direction):
 	path.add_point(position)
 
 
+# Attempts to move from the current path endpoint in the given direction
 func _leave_position(path: MovePath, direction):
 	var position = path.end_position
 	var inverse_direction = _invert_direction(direction)
@@ -79,6 +91,7 @@ func _leave_position(path: MovePath, direction):
 		_enter_position(to, path, direction)
 
 
+# Attempts to start a path at the given position
 func _start_at_position(position, path: MovePath):
 	var direction = MovePattern.NO_DIRECTION
 	if boundaries.is_in_bounds(position):
@@ -89,6 +102,7 @@ func _start_at_position(position, path: MovePath):
 		path.failed = true
 
 
+# Travel along the given path, interacting with obstacles along the way.
 func _travel_path(start, path: MovePath, iterations: int) -> Array:
 	var paths = []
 	var repeated = 1
@@ -113,8 +127,9 @@ func _travel_path(start, path: MovePath, iterations: int) -> Array:
 	return paths
 
 
+# Returns an array of completed paths based on the given MovePattern.
 func resolve_pattern(start_position, pattern: MovePattern, obstacles: Array = []) -> Array:
-	self.obstacles = obstacles
+	_obstacles = obstacles
 	var results := []
 	var paths = _pattern_to_paths(pattern)
 	for path in paths:
@@ -124,6 +139,7 @@ func resolve_pattern(start_position, pattern: MovePattern, obstacles: Array = []
 	return results
 
 
+# Returns an array of all points visited by the successful paths of a MovePattern.
 func get_travelled_points(start_position, pattern: MovePattern, obstacles: Array = []) -> Array:
 	var points = []
 	var paths = resolve_pattern(start_position, pattern, obstacles)
